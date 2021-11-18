dc9bd56fe51b08129fe1d92268896442fe616bba
su-tran75 <su-tran@hotmail.fr>
2021-11-18 20:07:50 +0800

    Create Settings.jsx add style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import TopBar from "./components/topBar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Write />
+      <Settings />
     </div>
   );
 }

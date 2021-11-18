cf5718463c6c6c9a17a9bbf3cb6622d56373352f
su-tran75 <su-tran@hotmail.fr>
2021-11-18 16:58:57 +0800

    Create Header.jsx and add style

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+import Header from "../../header/Header";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      Homepage
+    </div>
+  );
+}

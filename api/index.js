98a671379bb4c40f59c57b4f23000e7ec5cbe2fe
su-tran75 <su-tran@hotmail.fr>
2021-11-19 18:12:46 +0800

    Create register route

--- api/index.js
+++ api/index.js
@@ -2,25 +2,24 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
+// Able express to send json 
+app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use("/api/auth", authRoute);
+
 app.listen("5000", () => {
   console.log("Backend is running.");
 });
-
-
-
-// test if server is working within the terminal 
-// app.use("/", (req, res) => {
-//   console.log("This is the main url.")
-// })

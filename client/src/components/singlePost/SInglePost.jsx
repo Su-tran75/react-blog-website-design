23d0c315ee4f5ab990ffedb655c55965a2596da9
su-tran75 <su-tran@hotmail.fr>
2021-11-22 19:45:47 +0800

    Fetching single post using Axios

--- client/src/components/singlePost/SInglePost.jsx
+++ client/src/components/singlePost/SInglePost.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./singlePost.scss";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function SInglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    fetchPost();
+  }, [path]);
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img
-          className="singlePostImg"
-          src="https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-          alt=""
-        />
+        {post.photo && (
+          <img className="singlePostImg" src={post.photo} alt="" />
+        )}
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor sit amet.
+          {post.title}
           <div className="singlePostEdit">
             <i className="singlePostIcon far fa-edit"></i>
             <i className="singlePostIcon far fa-trash-alt"></i>
@@ -19,23 +31,13 @@ export default function SInglePost() {
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Author: <b>Léo</b>
+            Author: <b>{post.username}</b>
           </span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostDate">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-        <p className="singlePostDesc">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio odio,
-          fugit magni obcaecati recusandae, esse hic placeat, expedita saepe
-          animi doloribus cumque debitis dignissimos. Natus iusto ab eius cum
-          adipisci nulla animi ducimus ipsum ut? Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Labore aliquam doloremque quisquam
-          molestiae incidunt nulla numquam eligendi quae! Vero, quidem hic, esse
-          tempora ducimus ratione non labore consectetur minima at ad, expedita
-          repudiandae maiores pariatur. Esse modi cupiditate tenetur nihil quam
-          quae sit? Odit id doloribus voluptates iste sit animi. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Corporis in numquam nemo
-          iste, eaque perferendis voluptas repellendus sapiente ab ipsum.
-        </p>
+        <p className="singlePostDesc">{post.desc}</p>
       </div>
     </div>
   );
